1713faab27c44646dfef5a06345361bf2145d37e
JaxDanger <58536302+AltyS-CZ@users.noreply.github.com>
2023-04-25 14:28:35 -0400

    Update commands.lua

--- server/commands.lua
+++ server/commands.lua
@@ -4,8 +4,6 @@
 
 --INFO COMMAND (ALL)
 
--- THIS IS NEED FOR /INFO TO WORK
--- DO NOT REMOVE
 local function getPlayerData(player, callback)
     local playerId = tonumber(player)
     local identifiers = GetPlayerIdentifiers(playerId)
@@ -52,44 +50,7 @@ RegisterCommand("getpos", function(source, args, rawCommand)
         TriggerClientEvent("chat:addMessage", source, {args = {"^1[Server]", "^7You do not have permission to use this command."}})
     end
 end, true)
----------------------------
 
--- Command to spawn a vehicle by model name
-RegisterCommand("car", function(source, args, rawCommand)
-    -- Check if the player is an admin
-    if IsPlayerAceAllowed(source, "admin") then
-        -- Get the player's coordinates and heading
-        local playerPed = GetPlayerPed(source)
-        local playerPos = GetEntityCoords(playerPed)
-        local playerHeading = GetEntityHeading(playerPed)
-
-        -- Get the model name of the vehicle to spawn
-        local model = args[1]
-
-        -- Check if a model name was provided
-        if model ~= nil then
-            -- Request the model to be loaded
-            RequestModel(model)
-
-            -- Wait for the model to load
-            while not HasModelLoaded(model) do
-                Citizen.Wait(0)
-            end
-
-            -- Spawn the vehicle at the player's location and heading
-            local vehicle = CreateVehicle(model, playerPos, playerHeading, true, false)
-
-            -- Set the vehicle as the player's current vehicle
-            SetPedIntoVehicle(playerPed, vehicle, -1)
-
-            -- Send a message to the player that the vehicle was spawned
-            TriggerClientEvent("chat:addMessage", source, {color = {255, 255, 0}, args = {"Server", "Spawned " .. GetDisplayNameFromVehicleModel(model) .. " at your location."}})
-        else
-            -- Send an error message to the player if no model name was provided
-            TriggerClientEvent("chat:addMessage", source, {color = {255, 0, 0}, args = {"Error", "Invalid model name."}})
-        end
-    end
-end, true)
 
 
 ---------------------------
@@ -125,5 +86,19 @@ RegisterCommand('tpm', function(source, args)
         end
     else
         TriggerClientEvent('chat:addMessage', source, { args = { '^1Teleport', '^7Insufficient permission.' } })
+    end
+end, true)
+
+-- Define an Ace permission for the forcesalary command
+AddAceResourceCommand("custom_economy", "forcesalary", function(source, args, rawCommand)
+    -- Check that the player is authorized to run this command
+    if IsPlayerAceAllowed(source, "custom_economy.forcesalary") then
+        -- Trigger the GiveSalary() function to pay out salaries to all players
+        GiveSalary()
+        -- Send a confirmation message to the player who executed the command
+        TriggerClientEvent("chat:addMessage", source, { args = { "Economy", "Salaries have been forced for all players." }, color = { 255, 0, 0 } })
+    else
+        -- Send an error message to the player who executed the command if they're not authorized
+        TriggerClientEvent("chat:addMessage", source, { args = { "Economy", "You are not authorized to use this command." }, color = { 255, 0, 0 } })
     end
 end, true)
