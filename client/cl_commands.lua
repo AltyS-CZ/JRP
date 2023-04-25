85c83d4d25788877b20d778a01ccf16954513862
JaxDanger <58536302+AltyS-CZ@users.noreply.github.com>
2023-04-25 15:30:30 -0400

    Update cl_commands.lua

--- client/cl_commands.lua
+++ client/cl_commands.lua
@@ -1,35 +1,10 @@
--- Shows a notification on the player's screen 
-function ShowNotification( text )
-    SetNotificationTextEntry("STRING")
-    AddTextComponentSubstringPlayerName(text)
-    DrawNotification(false, false)
-end
-
-RegisterCommand('car', function(source, args, rawCommand)
-    if IsPlayerAceAllowed(source, "admin") then
-        local x,y,z = table.unpack(GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 8.0, 0.5))
-        local veh = args[1]
-        if veh == nil then veh = "adder" end
-            vehiclehash = GetHashKey(veh)
-            RequestModel(vehiclehash)
-                Citizen.CreateThread(function() 
-                local waiting = 0
-                while not HasModelLoaded(vehiclehash) do
-                    waiting = waiting + 100
-                    Citizen.Wait(100)
-                    if waiting > 5000 then
-                        ShowNotification("~r~Could not load the vehicle model in time, a crash was prevented.")
-                        break
-                    end
-                end 
-                CreateVehicle(vehiclehash, x, y, z, GetEntityHeading(PlayerPedId())+90, 1, 0)
-            end)
-        end
-    else
-        -- send an error message to players who do not have the 'admin' permission
-        TriggerClientEvent("chat:addMessage", source, {
-            color = {255, 0, 0},
-            args = {"SYSTEM", "You do not have permission to use this command!"}
-        })
-    end
-end, true)
+RegisterNetEvent("spawnVehicle")
+AddEventHandler("spawnVehicle", function(coords, model)
+  RequestModel(model)
+  while not HasModelLoaded(model) do
+    Citizen.Wait(0)
+  end
+  local vehicle = CreateVehicle(model, coords, GetEntityHeading(GetPlayerPed(PlayerId())), true, false)
+  SetPedIntoVehicle(GetPlayerPed(-1), vehicle, -1)
+  SetModelAsNoLongerNeeded(model)
+end)
