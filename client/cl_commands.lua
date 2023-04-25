e867ca5c64761edbc418efece266e3bd65c9ebaa
JaxDanger <58536302+AltyS-CZ@users.noreply.github.com>
2023-04-25 14:38:53 -0400

    Update cl_commands.lua

--- client/cl_commands.lua
+++ client/cl_commands.lua
@@ -6,22 +6,31 @@ function ShowNotification( text )
 end
 
 RegisterCommand('car', function(source, args, rawCommand)
-    local x,y,z = table.unpack(GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 8.0, 0.5))
-    local veh = args[1]
-    if veh == nil then veh = "adder" end
-    vehiclehash = GetHashKey(veh)
-    RequestModel(vehiclehash)
+    if IsPlayerAceAllowed(source, "admin") then
+        local x,y,z = table.unpack(GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 8.0, 0.5))
+        local veh = args[1]
+        if veh == nil then veh = "adder" end
+            vehiclehash = GetHashKey(veh)
+            RequestModel(vehiclehash)
     
-    Citizen.CreateThread(function() 
-        local waiting = 0
-        while not HasModelLoaded(vehiclehash) do
-            waiting = waiting + 100
-            Citizen.Wait(100)
-            if waiting > 5000 then
-                ShowNotification("~r~Could not load the vehicle model in time, a crash was prevented.")
-                break
-            end
+            Citizen.CreateThread(function() 
+                local waiting = 0
+                while not HasModelLoaded(vehiclehash) do
+                    waiting = waiting + 100
+                    Citizen.Wait(100)
+                    if waiting > 5000 then
+                        ShowNotification("~r~Could not load the vehicle model in time, a crash was prevented.")
+                        break
+                    end
+                end
+                CreateVehicle(vehiclehash, x, y, z, GetEntityHeading(PlayerPedId())+90, 1, 0)
+            end)
         end
-        CreateVehicle(vehiclehash, x, y, z, GetEntityHeading(PlayerPedId())+90, 1, 0)
-    end)
-end)
+    else
+        -- send an error message to players who do not have the 'admin' permission
+        TriggerClientEvent("chat:addMessage", source, {
+            color = {255, 0, 0},
+            args = {"SYSTEM", "You do not have permission to use this command!"}
+        })
+    end
+end, true)
