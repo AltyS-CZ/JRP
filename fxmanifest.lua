69eb5bad03f95a04f694bad4e80f929eeb780829
JaxDanger <58536302+AltyS-CZ@users.noreply.github.com>
2023-04-25 14:32:39 -0400

    Updated fxmanifest.lua
    Added comments to the shops files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,11 +9,15 @@ server_scripts {
     '@mysql-async/lib/MySQL.lua',
     'server/config_server.lua',
     'server/main.lua',
-    'server/commands.lua'
+    'server/commands.lua',
+    'server/jobs.lua',
+    --'server/cfg_shops.lua', -- Shops not ready yet
+    --'server/sv_shops.lua' -- Shops not ready yet
 }
 
 client_scripts {
-    'config.lua',
-    'client/main.lua'
+    'client/main.lua',
+    --'client/cl_shops.lua', -- Shops not ready yet
+    'client/cl_commands.lua'
 }
 
