6507083bea12eb2c42f20cddca93196f97be8fd0
JaxDanger <58536302+AltyS-CZ@users.noreply.github.com>
2023-05-21 20:53:55 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,17 +10,12 @@ server_scripts {
     'server/config_server.lua',
     'server/main.lua',
     'server/commands.lua',
-    'jrp_inv/config.lua',
-    'jrp_inv/items.lua',
-    'jrp_inv/server.lua'
 }
 
 client_scripts {
-    '@menuv/menuv.lua',
     'client/main.lua',
     'client/cmds/cl_car',
     'client/cl_commands.lua',
-    'jrp_inv/inventory.lua'
 }
 
 dependencies {
