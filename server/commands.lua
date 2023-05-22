f1ddfc3a03e76f437c424086abae00db86e2a286
JaxDanger <58536302+AltyS-CZ@users.noreply.github.com>
2023-05-21 20:34:15 -0400

    Update commands.lua

--- server/commands.lua
+++ server/commands.lua
@@ -118,3 +118,86 @@ RegisterCommand("dv", function(source, args, rawCommand)
         })
     end
 end, false)
+
+-----------------
+
+-- Command to give money to a player
+RegisterCommand("givemoney", function(source, args, rawCommand)
+    -- Check if the player has the 'admin' permission using ACE
+    if IsPlayerAceAllowed(source, "admin") then
+        -- Get the player's identifier
+        local identifier = GetPlayerIdentifier(source, 0)
+
+        -- Check if a valid player ID and money account were provided
+        if args[1] and args[2] and (args[2] == "cash" or args[2] == "bank") then
+            local player = tonumber(args[1])
+            local account = args[2]
+            local amount = tonumber(args[3])
+
+            -- Give money to the player's account
+            exports.custom_economy:addMoney(player, account, amount)
+
+            -- Notify the player about the money given
+            TriggerClientEvent('chat:addMessage', source, {
+                color = {0, 255, 0},
+                multiline = true,
+                args = {"Server", "Gave $" .. amount .. " to player " .. player .. "'s " .. account .. " account."}
+            })
+        else
+            -- Invalid arguments provided
+            TriggerClientEvent('chat:addMessage', source, {
+                color = {255, 0, 0},
+                multiline = true,
+                args = {"Server", "Invalid command usage. /givemoney [playerID] [cash/bank] [amount]"}
+            })
+        end
+    else
+        -- Player does not have permission to use the command
+        TriggerClientEvent('chat:addMessage', source, {
+            color = {255, 0, 0},
+            multiline = true,
+            args = {"Server", "You don't have permission to use this command."}
+        })
+    end
+end, false)
+
+-- Command to remove money from a player
+RegisterCommand("removemoney", function(source, args, rawCommand)
+    -- Check if the player has the 'admin' permission using ACE
+    if IsPlayerAceAllowed(source, "admin") then
+        -- Get the player's identifier
+        local identifier = GetPlayerIdentifier(source, 0)
+
+        -- Check if a valid player ID and money account were provided
+        if args[1] and args[2] and (args[2] == "cash" or args[2] == "bank") then
+            local player = tonumber(args[1])
+            local account = args[2]
+            local amount = tonumber(args[3])
+
+            -- Remove money from the player's account
+            exports.custom_economy:removeMoney(player, account, amount)
+
+            -- Notify the player about the money removed
+            TriggerClientEvent('chat:addMessage', source, {
+                color = {0, 255, 0},
+                multiline = true,
+                args = {"Server", "Removed $" .. amount .. " from player " .. player .. "'s " .. account .. " account."}
+            })
+        else
+            -- Invalid arguments provided
+            TriggerClientEvent('chat:addMessage', source, {
+                color = {255, 0, 0},
+                multiline = true,
+                args = {"Server", "Invalid command usage. /removemoney [playerID] [cash/bank] [amount]"}
+            })
+        end
+    else
+        -- Player does not have permission to use the command
+        TriggerClientEvent('chat:addMessage', source, {
+            color = {255, 0, 0},
+            multiline = true,
+            args = {"Server", "You don't have permission to use this command."}
+        })
+    end
+end, false)
+
