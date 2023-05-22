0ab4306b7e0178180261f89711ba5eaf7bea493e
JaxDanger <58536302+AltyS-CZ@users.noreply.github.com>
2023-05-21 21:02:45 -0400

    Update fxmanifest.lua

--- jrp_inv/fxmanifest.lua
+++ jrp_inv/fxmanifest.lua
@@ -2,18 +2,20 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'JaxDanger'
-description 'Inventory for JRP FW'
+description 'Inventory'
 version '0.0.1'
 lua54 'yes'
 
+shared_script '@ox_lib/init.lua'
+
 client_scripts {
-  '@menuv/menuv.lua',
+    '@menuv/menuv.lua',
 	'cl_inv.lua',
 }
 server_scripts {
-  'server.lua',
-  'items.lua',
-  'config.lua'
+    'server.lua',
+    'items.lua',
+    'config.lua'
 }
 
 dependencies {
