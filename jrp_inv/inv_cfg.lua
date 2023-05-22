deab75e0712a7484563a3aa3d691c878df3400c3
JaxDanger <58536302+AltyS-CZ@users.noreply.github.com>
2023-05-21 20:43:41 -0400

    Create inv_cfg.lua

---
+++ jrp_inv/inv_cfg.lua
@@ -0,0 +1,13 @@
+-- config.lua
+Config = {}
+
+Config.item_list = {
+    { item = "Apple", name = "Apple" },
+    { item = "Bread", name = "Bread" },
+    { item = "Water", name = "Water" },
+    { item = "Bandage", name = "Bandage" },
+    { item = "Soda", name = "Soda" }
+}
+
+
+return Config
