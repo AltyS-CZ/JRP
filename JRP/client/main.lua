2bf8a5b7b508a78f38266d02ade41d761e5b68a1
JaxDanger <58536302+Jax-Danger@users.noreply.github.com>
2023-05-22 21:24:46 -0400

    Update main.lua

--- JRP/client/main.lua
+++ JRP/client/main.lua
@@ -1,38 +1,3 @@
--- Function to update the money display
-function updateMoneyDisplay(cash, bank, dirtymoney)
-    SendNUIMessage({
-        type = "updateMoney",
-        cash = comma_value(cash),
-        bank = comma_value(bank),
-        dirtymoney = comma_value(dirtymoney)
-    })
-end
-
--- Event to update the money display
-RegisterNetEvent("custom_economy:updateMoneyDisplay")
-AddEventHandler("custom_economy:updateMoneyDisplay", function(cash, bank, dirtymoney)
-    updateMoneyDisplay(cash, bank, dirtymoney)
-end)
-
-
-function UpdateMoney()
-    Citizen.CreateThread(function()
-        while true do
-            Citizen.Wait(60000) -- wait for 1 minute
-            
-            -- Get the player's money and send it to the client
-            local player = GetPlayerFromServerId(source)
-            if player ~= -1 then
-                local money = GetPlayerMoney(player)
-                local bank = GetPlayerBank(player)
-                local dirtyMoney = GetPlayerDirtyMoney(player)
-                TriggerClientEvent("custom_economy:updateMoneyDisplay", player, money, bank, dirtyMoney)
-            end
-        end
-    end)
-end
-
-
 local PlayerData = {}
 
 function GetPlayerData()
@@ -43,27 +8,10 @@ function SetPlayerData(key, val)
     PlayerData[key] = val
 end
 
-function GetPlayerJob()
-    return PlayerData.job
-end
-
-function SetPlayerJob(job)
-    PlayerData.job = job
-end
-
-
--- Event handler to update the job display for the local player
-RegisterNetEvent("custom_economy:updateJobDisplay")
-AddEventHandler("custom_economy:updateJobDisplay", function(job)
-    PlayerData.job = job
-end)
-
-
-
 ------------------
 
-RegisterNetEvent('custom_economy:giveweapon')
-AddEventHandler('custom_economy:giveweapon', function(weapon)
+RegisterNetEvent('jrp:giveweapon')
+AddEventHandler('jrp:giveweapon', function(weapon)
     GiveWeaponToPed(GetPlayerPed(-1), GetHashKey(weapon), 1000, false, true)
 end)
 
