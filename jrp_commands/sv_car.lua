a60eb04bbca89dbc4ebc634be9985fe8246c0fbe
JaxDanger <58536302+Jax-Danger@users.noreply.github.com>
2023-05-22 22:37:32 -0400

    Update sv_car.lua

--- jrp_commands/sv_car.lua
+++ jrp_commands/sv_car.lua
@@ -1,5 +1,35 @@
 local Config = require('config')
 
+function isAdmin(source)
+    local identifiers = GetPlayerIdentifiers(source)
+    for _, id in ipairs(identifiers) do
+        if string.sub(id, 1, string.len("steam:")) == "steam:" then
+            for _, admin in ipairs(Config.admins) do
+                if id == admin then
+                    return true
+                end
+            end
+        end
+    end
+    return false
+end
+
+-- create a function to check if a player is a moderator
+function isModerator(source)
+    local identifiers = GetPlayerIdentifiers(source)
+    for _, id in ipairs(identifiers) do
+        if string.sub(id, 1, string.len("steam:")) == "steam:" then
+            for _, mod in ipairs(Config.moderators) do
+                if id == mod then
+                    return true
+                end
+            end
+        end
+    end
+    return false
+end
+
+
 
 RegisterCommand('car', function(source, args, rawCommand)
     -- check if the user is an admin or moderator
@@ -12,3 +42,5 @@ RegisterCommand('car', function(source, args, rawCommand)
         })
     end
 end, false)
+
+
