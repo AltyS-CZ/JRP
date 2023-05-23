8ee03cef62af7748eb25b7bb09f9286c9d410872
JaxDanger <58536302+Jax-Danger@users.noreply.github.com>
2023-05-22 21:15:15 -0400

    Update fxmanifest.lua

--- JRP/fxmanifest.lua
+++ JRP/fxmanifest.lua
@@ -7,14 +7,11 @@ version '0.0.5'
 
 server_scripts {
     '@mysql-async/lib/MySQL.lua',
-    'server/config_server.lua',
     'server/main.lua',
-    'server/commands.lua',
 }
 
 client_scripts {
     'client/main.lua',
-    'client/cmds/cl_car',
     'client/cl_commands.lua',
 }
 
