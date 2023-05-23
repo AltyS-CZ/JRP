f5efc130b389cf07894942fe50e767750bf10467
JaxDanger <58536302+Jax-Danger@users.noreply.github.com>
2023-05-22 21:40:36 -0400

    Update commands.lua

--- JRP/server/commands.lua
+++ JRP/server/commands.lua
@@ -1,30 +1,7 @@
 -------------------------
 
 --INFO COMMAND (ALL)
---- The function must be kept
--- in order for the info command to work
-local function getPlayerData(player, callback)
-    local playerId = tonumber(player)
-    local identifiers = GetPlayerIdentifiers(playerId)
-    local identifier = identifiers[1]
-    exports.oxmysql:execute('SELECT * FROM users WHERE identifier = ?', {identifier}, function(result)
-        if result[1] ~= nil then
-            local data = {
-                identifier = result[1].identifier,
-                license = result[1].license,
-                name = result[1].name,
-                job = result[1].job or "Citizen",
-                cash = result[1].cash,
-                bank = result[1].bank,
-                dirty_money = result[1].dirty_money,
-                position = json.decode(result[1].position) or nil
-            }
-            callback(data)
-        else
-            callback(nil)
-        end
-    end)
-end
+
 
 RegisterCommand("info", function(source, args)
     local player = source
