3ae2b0569c750731b0fdd0d2c37ac4426235df58
JaxDanger <58536302+Jax-Danger@users.noreply.github.com>
2023-05-26 15:29:37 -0400

    Update config.lua

--- jrp_inv/config.lua
+++ jrp_inv/config.lua
@@ -2,13 +2,32 @@
 Config = {}
 
 Config.item_list = {
-    { item = "Apple", name = "apple" },
-    { item = "Bread", name = "bread" },
-    { item = "Water", name = "water" },
-    { item = "Bandage", name = "bandage" },
-    { item = "Soda", name = "soda" }
-    -- Additional items follow format to work
+    categories = {
+        {
+            name = "Drinks",
+            items = {
+                { item = "Water", name = "Water" },
+                { item = "Soda", name = "Soda" }
+            }
+        },
+        {
+            name = "Food",
+            items = {
+                { item = "Apple", name = "Apple" },
+                { item = "Bread", name = "Bread" }
+            }
+        },
+        {
+            name = "Items",
+            items = {
+                { item = "Bandage", name = "Bandage" },
+                { item = "Phone", name = "Phone" },
+                { item = "Key", name = "Key" }
+            }
+        }
+    }
 }
+
 
 
 return Config
