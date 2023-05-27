87dabf52d2e7a78c1c5ecf171adb7ef4559b7ad7
JaxDanger <58536302+Jax-Danger@users.noreply.github.com>
2023-05-26 23:46:18 -0400

    Update main.lua

--- JRP/server/main.lua
+++ JRP/server/main.lua
@@ -1,37 +1,18 @@
-local oxmysql = exports.oxmysql
------------------------------------------------- PLEASE DO NOT TOUCH -----------------------------------------------------------------------------
--- Load player data on server start
-AddEventHandler('onResourceStart', function(resourceName)
-    if (GetCurrentResourceName() ~= resourceName) then
-        return
-    end
-    print('^5 JRP Framework has been sucessfully initialized.')
-end)
+-- Config
+local startingCash = 5000
+local startingBank = 5000
+local startingJob = 'unemployed'
 
--- Add new player to the database
+-- Event
 AddEventHandler('playerConnecting', function(playerName, setKickReason, deferrals)
-    local source = source
-    local identifiers = GetPlayerIdentifiers(source)
-    local identifier = identifiers[1]
+    local player = source
+    local identifier = GetPlayerIdentifiers(player)[1]
 
-    -- Check if player already exists in the database
-    MySQL.Async.fetchAll('SELECT * FROM users WHERE identifier = @identifier', {
-        ['@identifier'] = identifier
-    }, function(result)
+    exports.oxmysql:fetch('SELECT * FROM users WHERE identifier = ?', { identifier }, function(result)
         if result[1] == nil then
-            -- Add new player to the database
-            exports.oxmysql:execute('INSERT INTO users (identifier, job, cash, bank, dirty_money) VALUES (@identifier, @job, @cash, @bank, @dirty_money)', {
-                ['@identifier'] = identifier,
-                ["@job"] = "Citizen",
-                ['@cash'] = Config.StartingCash,
-                ['@bank'] = Config.StartingBank,
-                ['@dirty_money'] = 0
-            }, function(rowsChanged)
-                print('new user added to database')
-            end)
-        else
-            print('existing user found in database')
+            exports.oxmysql:execute('INSERT INTO users (identifier, cash, bank, dirty_money, job) VALUES (?, ?, ?, ?, ?)', {
+                identifier, startingCash, startingBank, 0, startingJob
+            })
         end
     end)
 end)
-
