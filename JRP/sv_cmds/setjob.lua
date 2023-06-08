cd7efb588eb0a16d54cdd6c255f80f50314448b7
JaxDanger <58536302+Jax-Danger@users.noreply.github.com>
2023-06-07 20:48:37 -0400

    Create setjob.lua

---
+++ JRP/sv_cmds/setjob.lua
@@ -0,0 +1,40 @@
+
+-------------- JOB STUFF DO NOT TOUCH -----------------
+-- Set default job
+local defaultJob = 'Unemployed'
+
+-- Command
+RegisterCommand('setjob', function(source, args, rawCommand)
+	if isAdmin(source) or isModerator(source) then
+    -- Check arguments
+	    if #args ~= 2 then
+    	    TriggerClientEvent('chat:addMessage', source, { args = {"^1[Error]", "Invalid number of arguments. Usage: /setjob [id] [job]"} })
+        	return
+    	end
+
+    -- Variables
+ 	   local targetId = args[1] -- Removed tonumber
+       local jobName = tostring(args[2])
+	
+    	if targetId and jobName then
+        -- Verify job exists
+        	exports.oxmysql:execute('SELECT * FROM job_list WHERE job = ?', {jobName}, function(result)
+            	if result and next(result) ~= nil then
+                -- Update job
+                	exports.oxmysql:execute('UPDATE users SET job = ? WHERE identifier = ?', {jobName, GetPlayerIdentifier(targetId, 0)}, function(affectedRows)
+                    	if affectedRows and next(affectedRows) ~= nil then
+                        	TriggerClientEvent('chat:addMessage', source, { args = {"^2[Success]", "Job updated successfully."} })
+	                    else
+    	                    TriggerClientEvent('chat:addMessage', source, { args = {"^1[Error]", "Failed to update job."} })
+        	            end
+            	    end)
+	            else
+    	            TriggerClientEvent('chat:addMessage', source, { args = {"^1[Error]", "Job does not exist."} })
+        	    end
+	        end)
+	    else
+    	    TriggerClientEvent('chat:addMessage', source, { args = {"^1[Error]", "Invalid arguments. Usage: /setjob [id] [job]"} })
+	    end
+	else
+		TriggerClientEvent('chat:addMessage', source, { args = {"^1[Error]", "You lack permission to use this command."} })
+end)
