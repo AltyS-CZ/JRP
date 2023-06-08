d0837cd5bab43269e5b919ba8228ae6f3b87e17e
JaxDanger <58536302+Jax-Danger@users.noreply.github.com>
2023-06-07 21:02:10 -0400

    Update sv_job.lua

--- JRP/sv_cmds/sv_job.lua
+++ JRP/sv_cmds/sv_job.lua
@@ -1,27 +1,24 @@
 RegisterCommand("job", function(source, args)
-	if isAdmin(source) or isModerator(source) then
-		local playerId = source
+    local playerId = source
     -- assuming that the identifier is steam hex
-	    local identifier = GetPlayerIdentifiers(playerId)[1] or false
+    local identifier = GetPlayerIdentifiers(playerId)[1] or false
 
-    	if not identifier then
-        	TriggerClientEvent('chat:addMessage', playerId, { args = { 'SYSTEM', 'Identifier not found.' } })
-        	return
-    	end
+    if not identifier then
+        TriggerClientEvent('chat:addMessage', playerId, { args = { 'SYSTEM', 'Identifier not found.' } })
+        return
+    end
 
-    	local query = 'SELECT job FROM users WHERE identifier = @identifier'
-    	local parameters = {
-        	['@identifier'] = identifier
-    	}
+    local query = 'SELECT job FROM users WHERE identifier = @identifier'
+    local parameters = {
+        ['@identifier'] = identifier
+    }
 
-    	exports.oxmysql:scalar(query, parameters, function(job)
-        	if not job then
-            	TriggerClientEvent('chat:addMessage', playerId, { args = { 'SYSTEM', 'No job found.' } })
-            	return
-        	end
+    exports.oxmysql:scalar(query, parameters, function(job)
+        if not job then
+            TriggerClientEvent('chat:addMessage', playerId, { args = { 'SYSTEM', 'No job found.' } })
+            return
+        end
 
-        	TriggerClientEvent('chat:addMessage', playerId, { args = { 'SYSTEM', 'Your job: ' .. job } })
-    	end)
-	end
+        TriggerClientEvent('chat:addMessage', playerId, { args = { 'SYSTEM', 'Your job: ' .. job } })
+    end)
 end, false)
-
