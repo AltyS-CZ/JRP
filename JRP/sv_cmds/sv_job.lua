ac8570d1d748b27c968a58a89c5e742d918d5089
JaxDanger <58536302+Jax-Danger@users.noreply.github.com>
2023-06-07 20:48:52 -0400

    Create sv_job.lua

---
+++ JRP/sv_cmds/sv_job.lua
@@ -0,0 +1,27 @@
+RegisterCommand("job", function(source, args)
+	if isAdmin(source) or isModerator(source) then
+		local playerId = source
+    -- assuming that the identifier is steam hex
+	    local identifier = GetPlayerIdentifiers(playerId)[1] or false
+
+    	if not identifier then
+        	TriggerClientEvent('chat:addMessage', playerId, { args = { 'SYSTEM', 'Identifier not found.' } })
+        	return
+    	end
+
+    	local query = 'SELECT job FROM users WHERE identifier = @identifier'
+    	local parameters = {
+        	['@identifier'] = identifier
+    	}
+
+    	exports.oxmysql:scalar(query, parameters, function(job)
+        	if not job then
+            	TriggerClientEvent('chat:addMessage', playerId, { args = { 'SYSTEM', 'No job found.' } })
+            	return
+        	end
+
+        	TriggerClientEvent('chat:addMessage', playerId, { args = { 'SYSTEM', 'Your job: ' .. job } })
+    	end)
+	end
+end, false)
+
